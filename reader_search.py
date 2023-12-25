7698649833773d6314bb4d465571d1258c5d1874
Enes <enescivan123@hotmail.com>
2023-12-25 19:34:23 +0300

    search function update
    search_str now takes 4 arguments which allows the user to search on a spesific chromosome and save the output

--- reader_search.py
+++ reader_search.py
@@ -2,30 +2,41 @@ def main():
     thingToSearch = input("What are you looking for in the database? ")
     #thingToSearch = "gene=SLC6A9"
     file_path = "cds_from_genomic.fna"
-    search_str(file_path, thingToSearch)
+    search_str(file_path, thingToSearch, 21, "output1")
 
-def search_str(file_path, word):
+def search_str(file_path, word, chrosomeNo, fileName):
     con = "y"
     nextGenom = 1
     rval = 0
+    geneToSave = ""
+    chrosomeSearch = ""
+    if chrosomeNo < 10:
+        chrosomeSearch = '0' + str(chrosomeNo)
     with open(file_path, 'r') as file:
         lines = file.readlines()
         rval = 0
         for line in lines:
-            if line.find(">lcl") != -1:
+            if line.find(">lcl|NC_0000" + chrosomeSearch) != -1:
                 rval = rval + 1
             if line.find(word) != -1 and con == "y":
                 print(word, 'exists in file')
                 print('Line Number:', lines.index(line) + 1)
                 print('Line:', line)
                 nextGenom = 1
-                while lines[lines.index(line) + nextGenom].find(">lcl") == -1:
+                geneToSave = lines[lines.index(line)]
+                while lines[lines.index(line) + nextGenom].find(">lcl|NC_0000" + chrosomeSearch) == -1:
                     print(lines[lines.index(line) + nextGenom], end="")
-                    if lines[lines.index(line) + nextGenom].find(">lcl") != -1:
+                    geneToSave += lines[lines.index(line) + nextGenom]
+                    if lines[lines.index(line) + nextGenom].find(">lcl|NC_0000" + chrosomeSearch) != -1:
                         break
                     nextGenom += 1
                 con = input("Print more results (y/n): ")
             if con == "n":
+                if fileName != "":
+                    file.close
+                    fileName += ".fna"
+                    with open(fileName, 'w') as file:
+                        file.write(geneToSave)
                 break
     file.close
     return rval
